81cb0510ff942e584af8be5464950b393f00a494
Richard Brown <flukefan@googlemail.com>
2017-07-19 09:54:03 +0100

    Added check that dependencies have not changed.

--- Reposify.Tests/DesignContraintsTests.cs
+++ Reposify.Tests/DesignContraintsTests.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Xml;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace Reposify.Tests
 {
@@ -10,57 +6,14 @@ namespace Reposify.Tests
     public class DesignContraintsTests
     {
         [Test]
-        public void PrivateAssemblies()
+        public void DependenciesHaveNotChanged()
         {
-            var knownPublicReferences = new string[]
+            var folder = @"..\..\..\_output";
+            var name = "Reposify";
+
+            NugetDependency.VerifyDependencies(folder, name, new string[]
             {
-                "Microsoft.CSharp",
-                "System",
-                "System.ComponentModel.DataAnnotations",
-                "System.Core",
-                "System.Data",
-                "System.Xml",
-                "System.Xml.Linq",
-                "System.Data.DataSetExtensions",
-            };
-
-            var packagesFolder = Path.GetFullPath(@"..\..\..\packages");
-
-            var csProjFiles =
-                Directory.GetFiles(@"..\..\..", "*.csproj", SearchOption.AllDirectories)
-                    .Select(f => Path.GetFullPath(f))
-                    .Where(f => !f.StartsWith(packagesFolder))
-                    .ToList();
-
-            var nonPrivateReferences = new List<string>();
-
-            foreach (var csProjFile in csProjFiles)
-            {
-                var projXml = new XmlDocument();
-                projXml.Load(csProjFile);
-
-                var references = projXml.SelectNodes("//*[local-name()='Reference']")
-                    .Cast<XmlElement>();
-
-                foreach (var reference in references)
-                {
-                    var assembly = reference.Attributes["Include"].Value;
-
-                    if (knownPublicReferences.Contains(assembly))
-                        continue;
-
-                    var privateNode = reference.ChildNodes
-                        .Cast<XmlElement>()
-                        .Where(n => n.Name == "Private")
-                        .SingleOrDefault();
-
-                    if (privateNode == null || privateNode.InnerText != "True")
-                        nonPrivateReferences.Add($"{csProjFile} has reference to assembly '{assembly}' that is not marked as private");
-                }
-            }
-
-            if (nonPrivateReferences.Count > 0)
-                Assert.Fail(string.Join("\n", nonPrivateReferences));
+            });
         }
     }
 }
