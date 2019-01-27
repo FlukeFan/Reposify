64423e3657a2fb23c05f11736d78c88d2bbdbee1
Richard Brown <fluke_fan@hotmail.com>
2019-01-27 16:13:42 +0000

    Added first stab at interface for LINQ queries.

---
+++ Reposify/IDbLinq.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Reposify
+{
+    public interface IDbLinq<TEntity, TResult>
+    {
+        TResult Execute(IQueryable<TEntity> queryable);
+    }
+}
---
+++ Reposify/IQueryableRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+namespace Reposify
+{
+    public interface IQueryableRepository
+    {
+        TResult         Execute<TEntity, TResult>(IDbLinq<TEntity, TResult> query);
+        IQueryable<T>   Query<T>();
+    }
+}
