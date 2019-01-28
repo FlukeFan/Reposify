cf4e4fc38cd7088fe64bb0943beca18c2dacfb7c
Richard Brown <fluke_fan@hotmail.com>
2019-01-28 15:16:57 +0000

    Added IDbExecutorAsync interface (not implemented yet).

---
+++ Reposify.Tests/IDbExecutorAsyncTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Reposify.Tests
+{
+    public abstract class IDbExecutorAsyncTests : IDisposableTests
+    {
+        private IRepository         Repository      { get => (IRepository)_disposable; }
+        private IDbExecutorAsync    DbExecutorAsync { get => (IDbExecutorAsync)_disposable; }
+
+        [Test]
+        public async virtual Task DbQuery_IsImplemented()
+        {
+            var poly1 = new PolyTypeBuilder().With(p => p.String, "poly1").Value();
+            var poly2 = new PolyTypeBuilder().With(p => p.String, "poly2").Value();
+            var poly3 = new PolyTypeBuilder().With(p => p.String, "poly3").Value();
+
+            var save = new QuerySaveEntities { EntitiesToSave = new[] { poly1, poly2, poly3 } };
+            await DbExecutorAsync.Execute(save);
+
+            var query = new QueryIn { IntValues = new[] { poly1.Id, poly3.Id } };
+            var entitiesWithId = await DbExecutorAsync.Execute(query);
+
+            entitiesWithId.Select(e => e.String).Should().BeEquivalentTo("poly1", "poly3");
+        }
+    }
+}
---
+++ Reposify/IDbExecutorAsync.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Reposify
+{
+    public interface IDbExecutorAsync
+    {
+        Task    Execute(IDbExecution dbExecution);
+        Task<T> Execute<T>(IDbQuery<T> dbQuery);
+    }
+}
