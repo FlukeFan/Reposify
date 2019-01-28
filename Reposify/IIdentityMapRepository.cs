2cfdd0d23ef702ed29e19e14c9fe1a48789610cf
Richard Brown <fluke_fan@hotmail.com>
2019-01-28 14:11:43 +0000

    Added TODO reminders.

--- Reposify/IIdentityMapRepository.cs
+++ Reposify/IIdentityMapRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace Reposify
 {
-    public interface IIdentityMapRepository : IRepository
+    public interface IIdentityMapRepository : IRepository // TODO: make this a separate interface without inheritance
     {
         void Clear();
     }
--- Reposify/IRepository.cs
+++ Reposify/IRepository.cs
@@ -5,6 +5,6 @@
         T       Save<T>(T entity)   where T : class, IEntity;
         T       Load<T>(object id)  where T : class, IEntity;
         void    Delete<T>(T entity) where T : class, IEntity;
-        void    Flush();
+        void    Flush(); // TODO: move to IUnitOfWork
     }
 }
