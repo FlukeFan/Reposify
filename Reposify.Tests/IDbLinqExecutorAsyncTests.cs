034f2a28be210381bbcdd744c4fb20c7a522b733
Richard Brown <fluke_fan@hotmail.com>
2019-01-29 10:06:47 +0000

    Added IDbLinqExecutorAsync interface.

---
+++ Reposify.Tests/IDbLinqExecutorAsyncTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Reposify.Tests
+{
+    public abstract class IDbLinqExecutorAsyncTests : IDisposableTests
+    {
+        private IRepository             Repository          { get => (IRepository)_disposable; }
+        private IDbLinqExecutorAsync    DbLinqExecutorAsync { get => (IDbLinqExecutorAsync)_disposable; }
+
+        [Test]
+        public async virtual Task Execute()
+        {
+            Repository.Save(new PolyTypeBuilder().With(p => p.String, "poly1").Value());
+            Repository.Save(new PolyTypeBuilder().With(p => p.String, "poly2").Value());
+            Repository.Save(new PolyTypeBuilder().With(p => p.String, "poly2").Value());
+            Repository.Save(new PolyTypeBuilder().With(p => p.String, "poly3").Value());
+
+            var count = await DbLinqExecutorAsync.ExecuteAsync(new QueryUsingLinq { StringValue = "poly2" });
+
+            count.Should().Be(2);
+        }
+    }
+}
---
+++ Reposify/IDbLinqExecutorAsync.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Reposify
+{
+    public interface IDbLinqExecutorAsync
+    {
+        Task<TResult> ExecuteAsync<TEntity, TResult>(IDbLinq<TEntity, TResult> query) where TEntity : class;
+    }
+}
